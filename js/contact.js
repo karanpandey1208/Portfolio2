cae5624517799584ebad33ffcffd114c0e9002b9
KARAN PANDEY <79134765+karanpandey1208@users.noreply.github.com>
2023-10-25 14:11:19 +0530

    Update contact.js
    added alert function for contact submit button

--- js/contact.js
+++ js/contact.js
@@ -4,3 +4,6 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+ function myFunction() {
+    alert("Message Sent!");
+  }
